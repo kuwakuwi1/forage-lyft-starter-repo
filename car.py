b38676d06bd2299d5e0cd8586abac28f4283b4cc
kuwakuwi1 <work.kuwakuwi@gmail.com>
2023-02-01 15:44:11 +0700

    add tires criteria to Car and CarFactory

--- car.py
+++ car.py
@@ -3,11 +3,13 @@ from abc import ABC
 from serviceable import Serviceable
 from engine.base import Engine
 from battery.base import Battery
+from tires.base import Tires
 
 class Car(Serviceable, ABC):
-    def __init__(self, engine: Engine, battery: Battery):
+    def __init__(self, engine: Engine, battery: Battery, tires: Tires):
         self.engine = engine
         self.battery = battery
+        self.tires = tires
 
     def needs_service(self) -> bool:
-        return self.engine.needs_service() or self.battery.needs_service()
+        return self.engine.needs_service() or self.battery.needs_service() or self.tires.needs_service()
--- car_factory.py
+++ car_factory.py
@@ -6,24 +6,46 @@ from engine.willoughby_engine import WilloughbyEngine
 from engine.sternman_engine import SternmanEngine
 from battery.splider_battery import SpliderBattery
 from battery.nubbin_battery import NubbinBattery
+from tires.carrigan_tires import CarriganTires
+from tires.octoprime_tires import OctoprimeTires
 
 class CarFactory:
     @staticmethod
-    def create_calliope(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
-        return Car(CapuletEngine(current_mileage, last_service_mileage), SpliderBattery(current_date, last_service_date))
+    def create_calliope(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int, tire_wear_sensors: list[float]) -> Car:
+        return Car(
+            CapuletEngine(current_mileage, last_service_mileage), 
+            SpliderBattery(current_date, last_service_date), 
+            CarriganTires(tire_wear_sensors)
+        )
 
     @staticmethod
-    def create_glissade(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
-        return Car(WilloughbyEngine(current_mileage, last_service_mileage), SpliderBattery(current_date, last_service_date))
+    def create_glissade(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int, tire_wear_sensors: list[float]) -> Car:
+        return Car(
+            WilloughbyEngine(current_mileage, last_service_mileage), 
+            SpliderBattery(current_date, last_service_date),
+            OctoprimeTires(tire_wear_sensors)
+        )
 
     @staticmethod
-    def create_palindrome(current_date: date, last_service_date: date, warning_light_on: bool) -> Car:
-        return Car(SternmanEngine(warning_light_on), SpliderBattery(current_date, last_service_date))
+    def create_palindrome(current_date: date, last_service_date: date, warning_light_on: bool, tire_wear_sensors: list[float]) -> Car:
+        return Car(
+            SternmanEngine(warning_light_on), 
+            SpliderBattery(current_date, last_service_date),
+            CarriganTires(tire_wear_sensors)
+        )
 
     @staticmethod
-    def create_rorschach(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car:
-        return Car(WilloughbyEngine(current_mileage, last_service_mileage), NubbinBattery(current_date, last_service_date))
+    def create_rorschach(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int, tire_wear_sensors: list[float]) -> Car:
+        return Car(
+            WilloughbyEngine(current_mileage, last_service_mileage), 
+            NubbinBattery(current_date, last_service_date),
+            OctoprimeTires(tire_wear_sensors)
+        )
 
     @staticmethod
-    def create_thovex(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int) -> Car: 
-        return Car(CapuletEngine(current_mileage, last_service_mileage), NubbinBattery(current_date, last_service_date))
+    def create_thovex(current_date: date, last_service_date: date, current_mileage: int, last_service_mileage: int, tire_wear_sensors: list[float]) -> Car: 
+        return Car(
+            CapuletEngine(current_mileage, last_service_mileage), 
+            NubbinBattery(current_date, last_service_date),
+            CarriganTires(tire_wear_sensors)
+        )
