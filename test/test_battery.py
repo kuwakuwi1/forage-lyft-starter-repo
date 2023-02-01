e3af2154e9cc3ee067f3fa518ad6357db1f2f57c
kuwakuwi1 <work.kuwakuwi@gmail.com>
2023-02-01 14:59:51 +0700

    add battery unittest(s)

---
+++ test/test_battery.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+import unittest
+from battery.nubbin_battery import NubbinBattery
+
+from battery.splider_battery import SpliderBattery
+
+class TestSpliderBattery(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+
+        battery = SpliderBattery(today, last_service_date)
+        self.assertTrue(battery.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 1)
+
+        battery = SpliderBattery(today, last_service_date)
+        self.assertFalse(battery.needs_service())
+
+class TestNubbinBattery(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 5)
+
+        battery = NubbinBattery(today, last_service_date)
+        self.assertTrue(battery.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+        today = datetime.today().date()
+        last_service_date = today.replace(year=today.year - 3)
+
+        battery = NubbinBattery(today, last_service_date)
+        self.assertFalse(battery.needs_service())
+
+if __name__ == '__main__':
+    unittest.main()
